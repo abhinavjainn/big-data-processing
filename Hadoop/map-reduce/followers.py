e8d9204d416c484fe2e17c0b5c0b6860443012a1
abhinavjainn <71486660+abhinavjainn@users.noreply.github.com>
2021-09-13 13:38:38 +0100

    Followers list using inverted index
    Indent messed up after upload.

--- Hadoop/map-reduce/followers.py
+++ Hadoop/map-reduce/followers.py
@@ -18,9 +18,9 @@ Here A follows both B and D; B follows A, C, D and E; etc.
 def mapper(self,follower,line_followed):      # Input - key: follower, value: list of nodes followed by key
 	list_followed = line_followed.split(“,”)  # Save the followed nodes in a list by splitting at “,”
 	for followed in list_followed:
-	yield (followed, follower)			      # Output: Followed node, follower node
+		yield (followed, follower)	# Output: Followed node, follower node
 
 
 # Reduce step: Collect all followers and output against the followed node
 def reducer(self,followed,followers): 		# input - key: followed node, value: follower node
-	yield(followed,followers) 				# output – followed node, followers
+	yield(followed,followers) 		# output – followed node, followers
